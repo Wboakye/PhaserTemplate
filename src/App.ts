b231cdfa6647c343a1e77914ab33779bc7d85b8b
William Boakye <wboakye@gmail.com>
2019-08-21 22:49:59 -0400

    initial commit

---
+++ src/App.ts
@@ -0,0 +1,17 @@
+export {};
+
+namespace App {
+
+    // game
+    export let game: Phaser.Game = null;
+}
+
+// -------------------------------------------------------------------------
+function launch(): void {
+
+    let game = new MaintainableGame.Game();
+    App.game = game;
+}
+
+// -------------------------------------------------------------------------
+window.onload = launch;
